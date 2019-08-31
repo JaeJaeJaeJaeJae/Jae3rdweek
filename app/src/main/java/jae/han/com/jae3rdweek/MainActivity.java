97d36e7baa3e7f13e70797c3c0e5ad3c1d9512a4
JaeJaeJaeJaeJae <2015085918@ust-ics.mygbiz.com>
2019-08-31 13:10:55 +0800

    Initial commit

---
+++ app/src/main/java/jae/han/com/jae3rdweek/MainActivity.java
@@ -0,0 +1,13 @@
+package jae.han.com.jae3rdweek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
